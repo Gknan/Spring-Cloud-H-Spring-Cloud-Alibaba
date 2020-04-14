d9187e5322ce5dd7a118ee1dec838bde87644ee7
gknan <1658590214@qq.com>
2020-04-14 16:17:15 +0800

    payment service as eureka client

--- cloud-provider-payment8001/src/main/java/cn/hust/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/hust/cloud/PaymentMain8001.java
@@ -2,10 +2,12 @@ package cn.hust.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * <h1>主启动类</h1>
  */
+@EnableEurekaClient // 开启 Eureka Client
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {

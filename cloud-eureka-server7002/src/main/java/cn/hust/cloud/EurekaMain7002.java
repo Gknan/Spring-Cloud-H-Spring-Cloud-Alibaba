e7916c30341dd5ac8005751a5c2a653b1ddb59b2
gknan <1658590214@qq.com>
2020-04-14 16:50:36 +0800

    add another eureka server for eureka group

---
+++ cloud-eureka-server7002/src/main/java/cn/hust/cloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package cn.hust.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <h1>Eureka 集群实例2</h1>
+ */
+@EnableEurekaServer // 开启 Eureka 服务端注解
+@SpringBootApplication
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}

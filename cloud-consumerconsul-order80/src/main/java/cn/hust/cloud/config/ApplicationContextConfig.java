3ff0db98776c2ee08e31c4c19bc0a9da47e1f151
gknan <1658590214@qq.com>
2020-04-14 20:47:24 +0800

    order service register to consul

---
+++ cloud-consumerconsul-order80/src/main/java/cn/hust/cloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package cn.hust.cloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 注入 RestTemplate 的配置类
+ *
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    // applicationContext.xml <bean id="" class="">
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}

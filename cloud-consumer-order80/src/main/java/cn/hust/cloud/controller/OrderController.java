3eb79046467b3d2d19eccdcbf3a758fcf098ee4b
gknan <1658590214@qq.com>
2020-04-15 10:59:26 +0800

    RestTemplate getForEntity&postForEntity

--- cloud-consumer-order80/src/main/java/cn/hust/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/hust/cloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package cn.hust.cloud.controller;
 import cn.hust.cloud.entities.CommonResult;
 import cn.hust.cloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,32 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id,
                 CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> retEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/"
+                + id, CommonResult.class);
+
+        if (retEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("template.getForEntity 调用成功");
+            return retEntity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
+    }
+
+    @GetMapping("/consumer/payment/create2") // 浏览器只能发 get 请求
+    public CommonResult<Payment> create2(Payment payment) {
+        // rul  请求参数  响应类型
+        ResponseEntity<CommonResult> responseEntity = restTemplate.postForEntity(PAYMENT_URL +
+                "/payment/create", payment, CommonResult.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("template.postForEntity 调用成功");
+            return responseEntity.getBody();
+        } else {
+            return new CommonResult<>(444, "创建失败");
+        }
     }
 }

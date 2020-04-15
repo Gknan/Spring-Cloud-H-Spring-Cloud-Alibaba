eb1e48b72c1ada4ca2a8c6e2dab01721023723cb
gknan <1658590214@qq.com>
2020-04-15 11:15:17 +0800

    customize ribbon loadbalancer

--- cloud-consumer-order80/src/main/java/cn/hust/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/hust/cloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package cn.hust.cloud;
 
+import cn.hust.customrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)// 指定访问的服务名和使用的负载均衡策略
 public class OrderMain80 {
 
     public static void main(String[] args) {
---
+++ cloud-consumer-order80/src/main/java/cn/hust/customrule/MySelfRule.java
@@ -0,0 +1,18 @@
+package cn.hust.customrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <h1>Ribbon 负载均衡策略替换</h1>
+ */
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule() {
+        return new RandomRule();// 定义为随机策略
+    }
+}

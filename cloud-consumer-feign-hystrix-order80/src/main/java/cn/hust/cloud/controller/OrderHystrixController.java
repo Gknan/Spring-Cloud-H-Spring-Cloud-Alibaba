722cd1220691d2b0a041da0e4f10921c6951be6d
gknan <1658590214@qq.com>
2020-04-15 17:06:24 +0800

    hystrix order consumer fallback

--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/hust/cloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/hust/cloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package cn.hust.cloud.controller;
 
 import cn.hust.cloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +24,15 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeotFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    }) // 消费者客户端兜底策略
     public String paymentInfo_timout(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfo_timout(id);
+    }
+
+    public String paymentInfoTimeotFallbackMethod(@PathVariable("id") Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + " 80 调用 8001 兜底策略返回";
     }
 
 }
--- cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/service/PaymentService.java
@@ -23,16 +23,16 @@ public class PaymentService {
 
     // 超过 3 s 降级；主启动类激活该注解
     @HystrixCommand(fallbackMethod = "paymentInfo_timoutHandler", commandProperties = {
-        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
     })
     public String paymentInfo_timout(Integer id) {
-        int timeNum = 5;
-//        try {
-//            TimeUnit.SECONDS.sleep(timeNum);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        int ret = 10 / 0; // 异常兜底
+        int timeNum = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNum);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        int ret = 10 / 0; // 异常兜底
         return "线程池: " +
                 Thread.currentThread().getName()
                 + " paymentInfo_timout, id" + id

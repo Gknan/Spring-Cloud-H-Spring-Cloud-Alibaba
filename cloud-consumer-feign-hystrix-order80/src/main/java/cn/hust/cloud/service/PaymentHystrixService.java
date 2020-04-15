c26636bacdd7cb7b4b2ddef925e852b0954b47bb
gknan <1658590214@qq.com>
2020-04-15 16:20:56 +0800

    build hystrix order consumer

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/hust/cloud/service/PaymentHystrixService.java
@@ -0,0 +1,21 @@
+package cn.hust.cloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * <h1>Payment 微服务业务接口</h1>
+ */
+@Component
+@FeignClient(value = "cloud-provider-hystrix-payment")
+public interface PaymentHystrixService {
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_timout(@PathVariable("id") Integer id);
+
+}

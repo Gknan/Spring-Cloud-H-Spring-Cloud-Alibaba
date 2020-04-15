0cee8bb74a7dd77ec400b19a3c19cf0b49f9ee02
gknan <1658590214@qq.com>
2020-04-15 16:44:53 +0800

    hystrix payment provider circuit breaker

--- cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/PaymentHystrixMain8001.java
@@ -2,10 +2,12 @@ package cn.hust.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker // 开启短路器
 public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/hust/cloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package cn.hust.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -19,16 +21,29 @@ public class PaymentService {
                 + " paymentInfo_OK, id" + id;
     }
 
+    // 超过 3 s 降级；主启动类激活该注解
+    @HystrixCommand(fallbackMethod = "paymentInfo_timoutHandler", commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_timout(Integer id) {
-        int timeNum = 3;
-        try {
-            TimeUnit.SECONDS.sleep(timeNum);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        int timeNum = 5;
+//        try {
+//            TimeUnit.SECONDS.sleep(timeNum);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int ret = 10 / 0; // 异常兜底
         return "线程池: " +
                 Thread.currentThread().getName()
                 + " paymentInfo_timout, id" + id
                 + " 耗时 " + timeNum + " s";
+    }
+
+    public String paymentInfo_timoutHandler(Integer id) {
+
+        return "线程池: " +
+                Thread.currentThread().getName()
+                + " paymentInfo_timoutHandler, id" + id
+                + " 服务提供方 8001 兜底方法";
     }
 }

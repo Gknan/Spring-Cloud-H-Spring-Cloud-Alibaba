12b46db7d99aae57cfc3ab4d1f134f2c4c6ecd89
gknan <1658590214@qq.com>
2020-04-17 12:09:42 +0800

    build config client module

---
+++ cloud-config-client-3355/src/main/java/cn/hust/cloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package cn.hust.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}

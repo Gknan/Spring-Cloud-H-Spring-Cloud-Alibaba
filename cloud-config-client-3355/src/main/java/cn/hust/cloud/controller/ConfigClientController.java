dc4c8450bb04c74cc06c55c88af932968616501b
gknan <1658590214@qq.com>
2020-04-17 12:51:36 +0800

    manually refresh config files

--- cloud-config-client-3355/src/main/java/cn/hust/cloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/cn/hust/cloud/controller/ConfigClientController.java
@@ -2,11 +2,13 @@ package cn.hust.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
